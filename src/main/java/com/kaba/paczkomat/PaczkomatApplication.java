b00b6b54c60c68c56563b32340d16869ea09f756
Daria Chukhlib <daria.chukhlib@play.pl>
2023-04-07 20:13:35 +0200

    initial commit

---
+++ src/main/java/com/kaba/paczkomat/PaczkomatApplication.java
@@ -0,0 +1,13 @@
+package com.kaba.paczkomat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaczkomatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaczkomatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaba/paczkomat/PaczkomatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaba.paczkomat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaczkomatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
